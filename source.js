0ace86f949e52ed612e89ba97e53cf6d4a2e6818
calebhoang <calebhoangm@gmail.com>
2025-12-06 16:51:14 -0600

    Added skeleton code in source.js

---
+++ source.js
@@ -0,0 +1,35 @@
+let num1 = NaN
+let num2 = NaN
+let func = NaN
+
+
+// Attempt to execute an operation
+function execute() {}
+
+// Functions
+function add() {}
+
+function subtract() {}
+
+function multiply() {}
+
+function divide() {}
+
+
+// Create number buttons
+function buildNumButtons() {}
+
+// Function called when a numButton is clicked
+function numClick() {}
+
+// Create function buttons
+function buildFunctionButtons() {}
+
+// Function called when a function button is clicked
+function funcClick() {}
+
+// Function called when the = button is clicked
+function equalsClick() {}
+
+// Function called when the c button is clicked
+function clearClick(){}
