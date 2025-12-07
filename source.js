9afc63caf9f0113fff87cdcafe93d74d5116a732
calebhoang <calebhoangm@gmail.com>
2025-12-07 14:19:12 -0600

    Created visual button/display layout

--- source.js
+++ source.js
@@ -7,29 +7,86 @@ let func = NaN
 function execute() {}
 
 // Functions
-function add() {}
+function add(val1, val2) {
+    if (isNaN(val1) || isNaN(val2)) {
+        return "ERROR"
+    }
+    return val1 + val2
+}
 
-function subtract() {}
+function subtract(val1, val2) {
+    if (isNaN(val1) || isNaN(val2)) {
+        return "ERROR"
+    }
+    return val1 - val2
+}
 
-function multiply() {}
+function multiply(val1, val2) {
+    if (isNaN(val1) || isNaN(val2)) {
+        return "ERROR"
+    }
+    return val1 * val2
+}
 
-function divide() {}
+function divide(val1, val2) {
+    if (isNaN(val1) || isNaN(val2)) {
+        return "ERROR"
+    }
+
+    if (val2 == 0) { // edge case for dividing by zero
+        return "Don't even think about it."
+    }
+    return val1 / val2
+}
 
 
-// Create number buttons
-function buildNumButtons() {}
+// create number buttons
+function buildNumButtons() {
+    const buttonsNumbers = document.querySelector(".buttonsNumbers")
+    for (let i = 0; i < 3; i ++) {
+        const buttonRow = document.createElement('div')
+        buttonRow.classList.add('numRow')
+        for (let j = 1; j <= 3; j++) {
+            const button = document.createElement('div')
+            button.classList.add('numButton')
+            button.textContent = i * 3 + j
+            buttonRow.appendChild(button)
+        }
+        buttonsNumbers.append(buttonRow)
+    }
+    const zeroRow = document.createElement('div')
+    zeroRow.classList.add('numRow')
+    const button = document.createElement('div')
+    button.classList.add('numButton')
+    button.textContent = 0
+    zeroRow.appendChild(button)
+    buttonsNumbers.appendChild(zeroRow)
+}
 
-// Function called when a numButton is clicked
+// function called when a numButton is clicked
 function numClick() {}
 
 // Create function buttons
-function buildFunctionButtons() {}
+function buildFunctionButtons() {
+    
+    const funcButtons = document.querySelector(".buttonsFunctions")
+    for (let i = 0; i < 5; i++) {
+        const button = document.createElement('div')
+        button.classList.add('funcButton')
+        funcButtons.appendChild(button)
+    }
+}
 
 // Function called when a function button is clicked
-function funcClick() {}
+function funcClick(value) {}
 
 // Function called when the = button is clicked
 function equalsClick() {}
 
 // Function called when the c button is clicked
-function clearClick(){}
+function clearClick() {
+
+}
+
+buildNumButtons()
+buildFunctionButtons()
